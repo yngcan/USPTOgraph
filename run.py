7aa7b808af1c1078db3641ad7559e5ccc14ca752
Ted Kornish <ted.kornish+github@gmail.com>
2014-03-07 17:51:02 -0700

    Improve run.py error handling

--- run.py
+++ run.py
@@ -63,10 +63,9 @@ commands = {
 
 try:
   cmd = sys.argv[1]
+  # Run command out of dictionary...so python run.py fetch will call fetch(), for example
+  commands[cmd.lower()]()
 except:
   print "Please try running `python run.py [arg]` one of the following:"
   print "\n".join(map(lambda x: "  - " + str(x), commands.keys()))
   sys.exit(1)
-
-# Run command out of dictionary...so python run.py fetch will call fetch(), for example
-commands[cmd.lower()]()
